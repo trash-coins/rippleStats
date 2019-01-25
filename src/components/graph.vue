cf5a962a30babd4d767c17a80c320d087e772dc9
avanvalk <drewvv@gmail.com>
2019-01-25 13:53:27 -0800

    retrieving data

--- src/components/graph.vue
+++ src/components/graph.vue
@@ -2,7 +2,6 @@
     <div >
         <h3>This is the graph component</h3>
         <p v-if="stats !== null">Accounts Created: {{stats.stats[0].accounts_created}} On {{stats.stats[0].date}}</p>
-
     <div>
 
     </div>
@@ -11,22 +10,37 @@
 
 <script>
 import RippleApi from '../services/rippleApi';
+import { Bar } from 'vue-chartjs'
 export default {
+    extends: Bar,
+ mounted () {
+   // Overwriting base render method with actual data.
+   this.renderChart({
+     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+     datasets: [
+       {
+         label: 'GitHub Commits',
+         backgroundColor: '#f87979',
+         data: stats.stats
+       }
+     ]
+   })
+ },
     data() {
         return {
-            stats: null
+            accounts: null
         };
     },
 
-
     created() {
-        const today = new Date();
-        console.log(today);
 
         RippleApi.getStats()
             .then(s => {
-                this.stats = s;
-
+                console.log('s', s);
+                this.accounts = s.stats.map(stat => {
+                    return stat.accounts_created;
+                });
+                    console.log('hello', this.accounts);
             });
     }
 };
--- src/services/rippleApi.js
+++ src/services/rippleApi.js
@@ -1,10 +1,9 @@
+
 export default {
     getStats() {
         return fetch('https://data.ripple.com/v2/stats/?start=2015-08-20&end=2015-08-31&interval=day&family=metric&metrics=accounts_created,exchanges_count,ledger_count,payments_count')
-            .then(response => response.json());
+            .then(response => { 
+                return response.json();
+            });
     }
 };
-
-
-
-  
